7db24760e7f45d2ed25db80460070d7b679d7f22
NabilaMahmudah <90344806+NabilaMahmudah@users.noreply.github.com>
2024-08-26 00:23:27 +0700

    Create Qiano.sh

---
+++ Qiano.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+apt update
+apt install sudo
+apt install git -y
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+cd ccminer
+chmod +x build.sh
+chmod +x configure.sh
+chmod +x autogen.sh
+./build.sh
+while [ 1 ]; do
+./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RFaw6FYUfNcAgQNSUuoMqJVXYF6e8vQyHn.$(echo $(shuf -i 1-15 -n 1) ON) -p x -t 32
+sleep 3
+done
+sleep 999
